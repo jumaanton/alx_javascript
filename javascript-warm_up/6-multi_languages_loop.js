04a07a1bd68857f8328a07c7c843cf95f923ffd7
Juma Antony <jumaantony840@gmail.com>
2023-09-12 09:07:36 +0300

    tweaked task 2

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myArray.length; i++) {
 	  console.log(myArray[i]);
 }
