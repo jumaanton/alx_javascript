723ba2b99fd21e7427b0cb244d53c976e558e746
Juma Antony <jumaantony840@gmail.com>
2023-09-29 11:49:17 +0300

    task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,22 @@
-const Square5 = require('./5-square');
+#!/usr/bin/node
 
-class Square extends Square5 {
-	  charPrint(c) {
-	if (c === undefined) {
-	  c = 'X';
-	}
-	for (let i = 0; i < this.height; i++) {
-	  console.log(c.repeat(this.width));
-	}
+const Rectangle = require('./5-square');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
-}
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      let row = '';
+      for (let j = 0; j < this.height; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+};
