5a7860d8f8e6974dbf4b50a97d3c3d6a8615daa1
Juma Antony <jumaantony840@gmail.com>
2023-09-29 11:30:51 +0300

    tweaked task 5

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -2,6 +2,19 @@ class Square extends Rectangle {
   constructor(size) {
 	super(w, h);
   }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += c;
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Square;
